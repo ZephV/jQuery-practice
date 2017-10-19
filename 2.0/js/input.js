c3317b56284f9f74820fbe07a7c541f0bcc26a77
ZephV <zephkwok@outlook.com>
2017-10-20 03:25:14 +0800

    add 2.0

---
+++ 2.0/js/input.js
@@ -0,0 +1,69 @@
+$(function(){
+	'use strict';
+
+	window.Input = function(selector){
+		var $ele
+			,$error_ele
+			,me = this
+			,rule =
+			{
+				required : true
+			};
+
+		function init() {
+            find_ele();
+            get_error_ele();
+            parse_rule();
+            me.load_validator();
+            listen();
+        }
+
+		this.load_validator = function(){
+			var val = this.get_val();
+			this.validator = new Validator(val,rule);
+		};
+
+		this.get_val = function(){
+			return $ele.val();
+		};
+
+        function find_ele() {
+            if (selector instanceof jQuery){
+                $ele = selector;
+            }else {
+                $ele = $(selector);
+            }
+        }
+
+        function parse_rule() {
+            var i;
+			var rule_string = $ele.data('rule');
+			var rule_arr = rule_string.split('|');
+			for (i = 0;i < rule_arr.length;i++) {
+                var item_str = rule_arr[i];
+                var item_arr = item_str.split(':');
+                rule[item_arr[0]] = JSON.parse(item_arr[1]);
+            }
+        }
+
+        function listen() {
+            $ele.on('blur',function(){
+                var valid = me.validator.is_valid(me.get_val());
+                if (valid)
+                    $error_ele.hide();
+				else
+                    $error_ele.show();
+
+            })
+        }
+
+        function get_error_ele(){
+        	$error_ele = $(get_error_selector());
+		}
+
+        function get_error_selector() {
+			return '#'+$ele.attr('name')+'-input-error';
+        }
+        init();
+	}
+}) ;
